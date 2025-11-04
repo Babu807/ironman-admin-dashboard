d3d7059eaa48b2fb16f47f56abc3efa679ed7e40
Babu807 <babukv807@gmail.com>
2025-11-04 22:39:05 +0530

    feat: env specific workflow change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: import.meta.env.VITE_BUILD_ENV === "server" ? "./" : "/ironman-admin-dashboard/",
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    base: env.VITE_BUILD_ENV === "server" ? "./" : "/ironman-admin-dashboard/",
+
+    plugins: [react()],
+  });
+};
