1bbe56f3b46fc88c33b74699a6a525597bd2f9fa
Babu807 <babukv807@gmail.com>
2025-11-04 22:28:24 +0530

    feat: env specific workflow change

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+const basename = import.meta.env.VITE_BUILD_ENV === "server" ? "/" : "/ironman-admin-dashboard";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ironman-admin-dashboard">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/ironman-admin-dashboard/',
+  base: import.meta.env.VITE_BUILD_ENV === "server" ? "./" : "/ironman-admin-dashboard/",
   plugins: [react()],
 })
