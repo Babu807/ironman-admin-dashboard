04458760d343ed6cc651e7660c8fb1466f3ad8d2
Babu807 <babukv807@gmail.com>
2025-11-26 21:06:25 +0530

    fix: host address change

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return defineConfig({
-    base: env.VITE_BUILD_ENV === "server" ? "./" : "/ironman-admin-dashboard/",
-
-    plugins: [react()],
-  });
-};
+export default defineConfig({
+  base: "/ironman-admin-dashboard/",
+  plugins: [react()],
+});
